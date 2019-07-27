76d88c6f0cfb6adf9c9b0b47dd3c83e053abb898
flori-software <52501549+flori-software@users.noreply.github.com>
2019-07-27 22:55:45 +0200

    Add files via upload

---
+++ Concentration/Concentration/Concentration.swift
@@ -0,0 +1,75 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Arkadiusz Paluszek on 30.05.19.
+//  Copyright © 2019 Arkadiusz Paluszek. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    private(set) var cards =  [Card]()
+    
+    private var indexOfOneAndOnlyFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                // Dieser Ausdruck sagt aus, dass cards[index] false sein soll, es sei denn, der index entspricht dem neuen Wert der in Set gesetzt wird
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index): chosen index not in the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            } else {
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of card")
+        for _ in  1...numberOfPairsOfCards {
+            let card = Card()
+            // 2 x - weil wir jede Karte 2 x brauchen: 2 x cards.append(card) oder alternativ:
+            cards += [card, card]
+            
+        }
+        // TODO: Shuffle the cards
+        var emojis = [Card]()
+        var freie_positionen = [Int]()
+        // Die zur Verfügung stehenden Positionen werden in ein Array geschrieben
+        for index in cards.indices {
+            freie_positionen.append(index)
+        }
+        var aktuelle_position = 0
+        while freie_positionen.count > 0 {
+            let zufallszahl = Int(arc4random_uniform(UInt32(freie_positionen.count - 1)))
+            emojis.append(cards[freie_positionen[zufallszahl]])
+            freie_positionen.remove(at: zufallszahl)
+            aktuelle_position += 1
+        }
+        cards = emojis
+    }
+}
---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Arkadiusz Paluszek on 30.05.19.
+//  Copyright © 2019 Arkadiusz Paluszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // "lazy" bedeutet, dass es erst initialisiert wird, wenn es tatsächlich zur Benutzung kommt
+    // An dieser Stelle steht nämlich cardButtons noch nicht zur Verfügung
+    private lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards: Int {
+            return (cardButtons.count + 2) / 2
+    }
+    private(set) var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet private weak var flipCountLabel: UILabel!
+    
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            flipCount += 1
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        } else {
+            print("Chosen card wasn't in card buttons")
+        }
+        
+        
+    }
+
+    private func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp {
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor = #colorLiteral(red: 0, green: 0.9810667634, blue: 0.5736914277, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0) : #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
+            }
+        }
+    }
+    
+    private var emojiChoices = ["😻", "😎", "😇", "😝", "🤪", "🕵🏽‍♀️", "👨🏼‍🌾", "👻", "👅", "👀"]
+    
+    // Die Emojis werden in ein Dictionary gepackt
+    private var emoji = [Int:String]()
+    
+    private func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        }
+        return emoji[card.identifier] ?? "?"
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
+        }
+    }
+}
