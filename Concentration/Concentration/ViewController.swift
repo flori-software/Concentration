7d3726e346908077d40b11533475134d0f248e95
Arkadiusz Paluszek <contact@flori-software.de>
2019-07-28 11:36:40 +0200

    Making Card hashable

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -56,13 +56,13 @@ class ViewController: UIViewController {
     private var emojiChoices = ["😻", "😎", "😇", "😝", "🤪", "🕵🏽‍♀️", "👨🏼‍🌾", "👻", "👅", "👀"]
     
     // Die Emojis werden in ein Dictionary gepackt
-    private var emoji = [Int:String]()
+    private var emoji = [Card:String]()
     
     private func emoji(for card: Card) -> String {
-        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-            emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+        if emoji[card] == nil, emojiChoices.count > 0 {
+            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
         }
-        return emoji[card.identifier] ?? "?"
+        return emoji[card] ?? "?"
     }
 }
 
